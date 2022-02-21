3cee797b043969ea2c39a9b6bccd62002362bb2c
belovanadin <req.of.hope@gmail.com>
2022-02-21 20:01:48 +0300

    деление на React-компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+ type AppProps = {
+   countOffer: number;
+ }
+
+function App({countOffer}: AppProps): JSX.Element {
+  return <Main countOffer={countOffer} />;
 }
 
 export default App;
