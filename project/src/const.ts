0015e4daad058ee52dab24cbf87a7cabf5ff2764
belovanadin <req.of.hope@gmail.com>
2022-03-20 15:44:16 +0300

    правка коммита

--- project/src/const.ts
+++ project/src/const.ts
@@ -12,3 +12,6 @@ export enum AutorizationStatus {
   NoAuth = 'NOAUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const URL_MARKER_DEFAULT = '../public/img/pin.svg';
+export const URL_MARKER_CURRENT = '../public/img/pin-active.svg';
---
+++ project/src/mocks/city.ts
@@ -0,0 +1,8 @@
+import { City } from '../types/offer';
+
+export const CITY: City = {
+  title: 'Амстердам',
+  lat: 52.370216,
+  lng: 4.895168,
+  zoom: 10,
+};
